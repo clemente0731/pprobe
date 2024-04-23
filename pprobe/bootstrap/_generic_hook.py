df304d0a0f90066f4f54475908215a76f762c386
clemente0731 <clemente0620@gmail.com>
2024-04-23 17:41:33 +0800

    add torch.distributed

--- pprobe/bootstrap/_generic_hook.py
+++ pprobe/bootstrap/_generic_hook.py
@@ -3,6 +3,7 @@ import importlib
 import sys
 import time
 from .logging import Logger
+# import torch
 _hook_modules = {'torch'}
 func_counts = 0
 
@@ -34,7 +35,7 @@ class MetaPathLoader:
 
 sys.meta_path.insert(0, MetaPathFinder())
 
-def func_count_wrapper(func):
+def func_torch_step_count_wrapper(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         global func_counts
@@ -50,7 +51,45 @@ def func_count_wrapper(func):
             Logger.warn(f"func:{func} is not callable")
     return wrapper
 
+def func_torch_distributed_wrapper(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if callable(func):
+            result = func(*args, **kwargs)
+            if isinstance(args, tuple):
+                args_info = ", ".join([f"args_{idx} shape {arg.shape}, dtype {arg.dtype} " for idx, arg in enumerate(args)])
+                Logger.info(f"[PPROBE] torch.distributed.{func.__qualname__} {args_info}, kwargs {kwargs}")
+            else:
+                Logger.info(f"[PPROBE] torch.distributed.{func.__qualname__} args {args}, kwargs {kwargs} ")
+            return result
+        else:
+            # 处理 func 不可调用的情况
+            Logger.warn(f"func:{func} is not callable")
+    return wrapper
+
 def module_hook(fullname, module):
     if fullname == "torch":
         # torch.Tensor.backward 和 torch.autograd.backward 是等价的
-        module.autograd.backward = func_count_wrapper(module.autograd.backward)
+        module.autograd.backward = func_torch_step_count_wrapper(module.autograd.backward)
+
+        # torch.distributed part
+        module.distributed.broadcast = func_torch_distributed_wrapper(module.distributed.broadcast)
+        module.distributed.all_reduce = func_torch_distributed_wrapper(module.distributed.all_reduce)
+        module.distributed.reduce = func_torch_distributed_wrapper(module.distributed.reduce)
+        module.distributed.all_gather = func_torch_distributed_wrapper(module.distributed.all_gather)
+        module.distributed.gather = func_torch_distributed_wrapper(module.distributed.gather)
+        module.distributed.scatter = func_torch_distributed_wrapper(module.distributed.scatter)
+        module.distributed.reduce_scatter = func_torch_distributed_wrapper(module.distributed.reduce_scatter)
+        module.distributed.send = func_torch_distributed_wrapper(module.distributed.send)
+        module.distributed.recv = func_torch_distributed_wrapper(module.distributed.recv)
+
+        # torch.distributed.broadcast(tensor, src, group=None, async_op=False)
+        # torch.distributed.all_reduce(tensor, op=<RedOpType.SUM: 0>, group=None, async_op=False)
+        # torch.distributed.reduce(tensor, dst, op=<RedOpType.SUM: 0>, group=None, async_op=False)
+        # torch.distributed.all_gather(tensor_list, tensor, group=None, async_op=False)
+        # torch.distributed.gather(tensor, gather_list=None, dst=0, group=None, async_op=False)
+        # torch.distributed.scatter(tensor, scatter_list=None, src=0, group=None, async_op=False)
+        # torch.distributed.reduce_scatter(output, input_list, op=<RedOpType.SUM: 0>, group=None, async_op=False)
+        # torch.distributed.send(tensor, dst, group=None, tag=0)
+        # torch.distributed.recv(tensor, src=None, group=None, tag=0)
+        # torch.distributed.barrier(group=None, async_op=False, device_ids=None)
