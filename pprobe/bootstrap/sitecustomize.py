25957bb399b63eec65ce569b790158ff4173b8ee
host452b <32806348+host452b@users.noreply.github.com>
2024-05-29 16:56:25 +0800

    refine pprobe call env var

--- pprobe/bootstrap/sitecustomize.py
+++ pprobe/bootstrap/sitecustomize.py
@@ -6,15 +6,20 @@ def check_and_run_hook():
     otherwise, print a warning message (only once).
     """
     # Check if PPROBE is enabled by checking the environment variable
-    pprobe_enabled = bool(os.environ.get("PPROBE_ENABLE"))
+    pprobe_enabled = int(os.environ.get("PPROBE_ENABLE", 0))
 
-    if pprobe_enabled:
-        # Import and run the hook only if PPROBE is enabled
+    if pprobe_enabled and pprobe_enabled == 1:
+        # generic hook
         from pprobe.bootstrap import _generic_hook
+    elif pprobe_enabled and pprobe_enabled == 2:
+        # torch func hook
+        from pprobe.bootstrap import torchfunc_hook
+        context=torchfunc_hook.TorchFunctionContext()
+        context.__enter__()
     else:
         # Print the warning message only once
         if not getattr(check_and_run_hook, 'warning_printed', False):
-            print("[PPROBE] Please set the environment variable PPROBE_ENABLE=1 to use pprobe.")
+            print("[PPROBE] Please set the environment variable PPROBE_ENABLE=1/2/3/4 to use pprobe.")
             setattr(check_and_run_hook, 'warning_printed', True)
 
 # Call the function to check and run the hook
