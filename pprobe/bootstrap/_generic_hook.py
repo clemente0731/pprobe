9d52b3ba78ddebfd1a2d6af5f5e2550012821c8a
host452b <32806348+host452b@users.noreply.github.com>
2024-05-29 16:38:51 +0800

    feat: Add device conversion detection

--- pprobe/bootstrap/_generic_hook.py
+++ pprobe/bootstrap/_generic_hook.py
@@ -2,10 +2,21 @@ import functools
 import importlib
 import sys
 import time
+import traceback
 from .logging import Logger
 # import torch
 _hook_modules = {'torch'}
 func_counts = 0
+
+def trace_function_call():
+    try:
+        raise Exception("Trace function call")
+    except:
+        # 获取调用堆栈信息
+        stack_trace = traceback.extract_stack()
+        # 打印堆栈信息
+        for stack_entry in stack_trace[:-2]:
+            Logger.warn(f"\t\t Trace File: {stack_entry.filename}, Line: {stack_entry.lineno}")
 
 class MetaPathFinder:
 
@@ -28,7 +39,7 @@ class MetaPathLoader:
         finder = sys.meta_path.pop(0)
         module = importlib.import_module(fullname)
         Logger.info(f"META-PATH-LOADER --> MODULE {module}")
-        module_hook(fullname, module)
+        torch_hook_fn(fullname, module)
         sys.meta_path.insert(0, finder)
 
         return module
@@ -47,7 +58,28 @@ def func_torch_step_count_wrapper(func):
             Logger.info(f"[PPROBE] func_name {func} --> counts {func_counts}")
             return result
         else:
-            # 处理 func 不可调用的情况
+            # handle the case where func is not callable
+            Logger.warn(f"func:{func} is not callable")
+    return wrapper
+
+def func_torch_device_conversion_wrapper(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if callable(func):
+            tensor_ret = func(*args, **kwargs)
+
+            if func.__name__ == "to":
+                trace_function_call()
+                Logger.warn(f"[PPROBE] find device conversion call {func}, The tensor is conversion to {str(tensor_ret.device)}")
+            elif func.__name__ == "cpu":
+                trace_function_call()
+                Logger.warn(f"[PPROBE] find device conversion call {func}, The tensor is on CPU")
+            elif func.__name__ == "cuda":
+                Logger.info(f"[PPROBE] find device conversion call {func}, The tensor is on {str(tensor_ret.device)}")
+
+            return tensor_ret
+        else:
+            # handle the case where func is not callable
             Logger.warn(f"func:{func} is not callable")
     return wrapper
 
@@ -66,25 +98,30 @@ def func_torch_distributed_wrapper(func):
                 Logger.info(f"[PPROBE] torch.distributed.{func.__qualname__} args {args}, kwargs {kwargs} ")
             return result
         else:
-            # 处理 func 不可调用的情况
+            # handle the case where func is not callable
             Logger.warn(f"func:{func} is not callable")
     return wrapper
 
-def module_hook(fullname, module):
+def torch_hook_fn(fullname, module):
     if fullname == "torch":
-        # torch.Tensor.backward 和 torch.autograd.backward 是等价的
+        ###################################################
+        # torch.Tensor.backward 和 torch.autograd.backward
+        ###################################################
         module.autograd.backward = func_torch_step_count_wrapper(module.autograd.backward)
 
-        # torch.distributed part
-        module.distributed.broadcast = func_torch_distributed_wrapper(module.distributed.broadcast)
-        module.distributed.all_reduce = func_torch_distributed_wrapper(module.distributed.all_reduce)
-        module.distributed.reduce = func_torch_distributed_wrapper(module.distributed.reduce)
-        module.distributed.all_gather = func_torch_distributed_wrapper(module.distributed.all_gather)
-        module.distributed.gather = func_torch_distributed_wrapper(module.distributed.gather)
-        module.distributed.scatter = func_torch_distributed_wrapper(module.distributed.scatter)
-        module.distributed.reduce_scatter = func_torch_distributed_wrapper(module.distributed.reduce_scatter)
-        module.distributed.send = func_torch_distributed_wrapper(module.distributed.send)
-        module.distributed.recv = func_torch_distributed_wrapper(module.distributed.recv)
+        ###################################################
+        ## torch.distributed part
+        ###################################################
+
+        # module.distributed.broadcast = func_torch_distributed_wrapper(module.distributed.broadcast)
+        # module.distributed.all_reduce = func_torch_distributed_wrapper(module.distributed.all_reduce)
+        # module.distributed.reduce = func_torch_distributed_wrapper(module.distributed.reduce)
+        # module.distributed.all_gather = func_torch_distributed_wrapper(module.distributed.all_gather)
+        # module.distributed.gather = func_torch_distributed_wrapper(module.distributed.gather)
+        # module.distributed.scatter = func_torch_distributed_wrapper(module.distributed.scatter)
+        # module.distributed.reduce_scatter = func_torch_distributed_wrapper(module.distributed.reduce_scatter)
+        # module.distributed.send = func_torch_distributed_wrapper(module.distributed.send)
+        # module.distributed.recv = func_torch_distributed_wrapper(module.distributed.recv)
 
         # torch.distributed.broadcast(tensor, src, group=None, async_op=False)
         # torch.distributed.all_reduce(tensor, op=<RedOpType.SUM: 0>, group=None, async_op=False)
@@ -96,3 +133,10 @@ def module_hook(fullname, module):
         # torch.distributed.send(tensor, dst, group=None, tag=0)
         # torch.distributed.recv(tensor, src=None, group=None, tag=0)
         # torch.distributed.barrier(group=None, async_op=False, device_ids=None)
+
+        ###################################################
+        ## torch.Tensor.to part
+        ###################################################
+        module.Tensor.to = func_torch_device_conversion_wrapper(module.Tensor.to)
+        module.Tensor.cpu = func_torch_device_conversion_wrapper(module.Tensor.cpu)
+        module.Tensor.cuda = func_torch_device_conversion_wrapper(module.Tensor.cuda)
