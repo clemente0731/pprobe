94ee809ac92d3a99b38cdcc52ad1a913471e9533
host452b <32806348+host452b@users.noreply.github.com>
2024-08-08 08:26:34 +0000

    add hook for dataloader

--- pprobe/bootstrap/hooks/pytorch_catch.py
+++ pprobe/bootstrap/hooks/pytorch_catch.py
@@ -1,14 +1,13 @@
 import time
 import functools
 from pprobe.utils.logging import Logger
+from typing import Any, Callable
 
 func_counts = 0
-
-
 def func_torch_step_count_wrapper(func):
-    ###################################################
-    # torch.autograd.backward / torch.Tensor.backward
-    ###################################################
+    """
+    torch.autograd.backward / torch.Tensor.backward
+    """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         global func_counts
@@ -18,10 +17,21 @@ def func_torch_step_count_wrapper(func):
             result = func(*args, **kwargs)
             end = time.time()
             func_counts += 1
-            Logger.info(f"[PPROBE] func_name {func} --> counts {func_counts}")
+            Logger.info(f"[PPROBE] training func_name {func} --> counts {func_counts}")
             return result
         else:
             # handle the case where func is not callable
             Logger.warn(f"func:{func} is not callable")
 
+    return wrapper
+
+
+def dataloader_next_method_wrapper(original_next: Callable) -> Callable:
+    """
+    Decorator function to wrap the original __next__ method and add additional debug information.
+    """
+    def wrapper(self) -> Any:
+        Logger.info("[PPROBE] Iteration count ===>:", getattr(self, '_num_yielded', 'N/A'))
+        return original_next(self)
+    
     return wrapper
