2c939d6ea5f5560c914828a78b9fbf7d083abff7
clemente0731 <32806348+clemente0731@users.noreply.github.com>
2024-08-08 10:30:15 +0000

    update lr hook

--- pprobe/bootstrap/hooks/pytorch_optim.py
+++ pprobe/bootstrap/hooks/pytorch_optim.py
@@ -2,22 +2,15 @@ from pprobe.utils.logging import Logger
 from typing import Any, Callable, Optional
 
 
-def lr_scheduler_step_method_wrapper(
-    original_method: Callable, epoch: Optional[int] = None
-):
-    def wrapper(self) -> Any:
-        lr = self.get_lr()
-        Logger.info(f"[PPROBE] INIT LR ===> {lr}")
-        return original_method(self, epoch)
-
-    return wrapper
-
-
 def optimizer_zero_grad_method_wrapper(
     original_method, set_to_none: bool = True
 ) -> None:
     def wrapper(self) -> Any:
         for group in self.param_groups:
+            """
+            group:
+                dict_keys(['params', 'lr', 'momentum', 'dampening', 'weight_decay', 'nesterov', 'maximize', 'foreach', 'differentiable', 'fused', 'initial_lr'])
+            """
             lr = group["lr"]
             Logger.info(f"[PPROBE] Iteration optimizer lr ===> {lr}")
         return original_method(self, set_to_none)
